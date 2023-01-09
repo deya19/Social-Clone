57f71c89bc63cf755c0993bd7180f9309eb0cf1b
deya19 <deyasaleh2@gmail.com>
2023-01-09 21:11:31 +0300

    new update

--- api/connect.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql";
-
-export const db = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"Deyasaleh@12",
-  database:"social"
-})
--- api/index.js
+++ api/index.js
@@ -1,26 +1,76 @@
 import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import commentRoutes from "./routes/comments.route.js";
+import likeRoutes from "./routes/likes.route.js";
+import postRoutes from "./routes/post.route.js";
+import userRoutes from "./routes/users.route.js";
+import relatioshipRoutes from "./routes/relationships.route.js";
+import storyRoutes from "./routes/stories.route.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import dotenv from "dotenv";
+dotenv.config();
+import rawBody from "raw-body";
+import contentType from "content-type";
+import bodyParser from "body-parser";
+import hpp from "hpp";
+import helmet from "helmet";
+import loggerServices from "./services/logger.sevice.js";
+
+
+//Winston Logger
+const logger = new loggerServices("error.control");
+const port = process.env.SERVER_PORT;
 const app = express();
-import authRoutes from "./routes/auth.js"
-import commentRoutes from "./routes/comments.js"
-import likeRoutes from "./routes/likes.js"
-import postRoutes from "./routes/post.js"
-import userRoutes from "./routes/users.js"
-import relatioshipRoutes from "./routes/relationships.js"
-import storyRoutes from "./routes/stories.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import multer from "multer"
+
+//setting various HTTP headers
+app.use(helmet());
+
+// security vulnerability that occurs when an attacker is able to inject multiple values into a single HTTP request parameter, resulting in unexpected behavior or exploitation of a web application.
+app.use(hpp());
+
 
 //middlewares
 app.use((req,res,next)=>{
   res.header("Access-Control-Allow-Credentials",true);
   next();
-})
-app.use(express.json())
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// to reduce the lenght of request
+app.use((req, res, next) => {
+  if (req.readable) {
+    // read the request body and attach it to req.text
+    rawBody(req, {
+      length: req.headers['content-length'],
+      limit: '1mb',
+      encoding: contentType.parse(req).parameters.charset
+    }, (err, string) => {
+      if (err) {
+        logger.error("the lenght of request too long" + JSON.stringify(err));
+        return next(err);
+      };
+      req.text = string;
+      next();
+    });
+  } else {
+    // the request body is not readable, so skip parsing it
+    next();
+  }
+});
+
+
 app.use(cors({
   origin:"http://localhost:3000",
-}))
-app.use(cookieParser())
+}));
+
+//It allows you to read, parse, and set cookies in a Node.js application.
+app.use(cookieParser());
 
 const storage = multer.diskStorage({
   //path to storage pic in client file
@@ -52,6 +102,9 @@ app.use("/api/users" , userRoutes)
 
 
 
-app.listen(8800,() => {
-  console.log("API Working!")
-})
+app.listen(port,() => {
+  console.log(`Server Start .......... `);
+});
+
+export default app;
+
